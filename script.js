1fd30ba9c1e97e1a45feea26ac80cf2378cebc54
arminpaul17-maker <arminpaul17@gmail.com>
2026-01-31 20:23:56 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const finalScreen = document.getElementById("finalScreen");
+const mainScreen = document.querySelector(".center");
+
+yesBtn.addEventListener("click", () => {
+    mainScreen.classList.add("hidden");
+    finalScreen.classList.remove("hidden");
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const randomX = Math.random() * 200 - 100;
+    const randomY = Math.random() * 200 - 100;
+    noBtn.style.transform = `translate(${randomX}px, ${randomY}px)`;
